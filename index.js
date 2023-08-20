bbccb3b29f7191affd96bfed62a9c1e15cce7d32
CyndiCodes <huertascyn@gmail.com>
2023-08-19 23:45:11 -0400

    index.js now has varibles and method calls instead of other js files. other js files should only hold the classes saving for the evening

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const fs = require("fs");
 let SVG = require("./lib/svg");
 const Text = require("./lib/text")
-// const {Shape, Circle, Triangle, Square} = require("./lib/shapes")
+const {Shape, Circle, Triangle, Square} = require("./lib/shapes")
 
-let shapeChoice = new SVG("square")
+let shapeChoice = new SVG("circle")
+let text = new Text("CYN", "purple")
+let shapeColor = new Shape("green", "black", "5")
 
 let writeData = `<svg version="1.1"
          width="300" height="200"
          xmlns="http://www.w3.org/2000/svg">
 
-         ${shapeChoice.render()}       
+         <${shapeChoice.render()} ${shapeColor.render()}/>     
 
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+         ${text.render()}
     
         </svg>`
+
 
 fs.writeFile("shape.svg", writeData, function(err){
     if(err)
